2004c2b3f6e9e87ddf323ef22774e64397ee6fb7
masa.tseng <af24520985@gmail.com>
2025-04-27 22:56:28 +0800

    Initial flutter n puzzle game  with level 3.

--- lib/main.dart
+++ lib/main.dart
@@ -1,3 +1,4 @@
+import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
 
 void main() {
@@ -7,46 +8,22 @@ void main() {
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
 
-  // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
+      title: 'N Puzzle Game',
       theme: ThemeData(
-        // This is the theme of your application.
-        //
-        // TRY THIS: Try running your application with "flutter run". You'll see
-        // the application has a purple toolbar. Then, without quitting the app,
-        // try changing the seedColor in the colorScheme below to Colors.green
-        // and then invoke "hot reload" (save your changes or press the "hot
-        // reload" button in a Flutter-supported IDE, or press "r" if you used
-        // the command line to start the app).
-        //
-        // Notice that the counter didn't reset back to zero; the application
-        // state is not lost during the reload. To reset the state, use hot
-        // restart instead.
-        //
-        // This works for code too, not just values: Most code changes can be
-        // tested with just a hot reload.
-        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
+        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepOrange),
         useMaterial3: true,
       ),
-      home: const MyHomePage(title: 'Flutter Demo Home Page'),
+      home: const MyHomePage(title: 'N Puzzle Game'),
+      debugShowCheckedModeBanner: false,
     );
   }
 }
 
 class MyHomePage extends StatefulWidget {
   const MyHomePage({super.key, required this.title});
-
-  // This widget is the home page of your application. It is stateful, meaning
-  // that it has a State object (defined below) that contains fields that affect
-  // how it looks.
-
-  // This class is the configuration for the state. It holds the values (in this
-  // case the title) provided by the parent (in this case the App widget) and
-  // used by the build method of the State. Fields in a Widget subclass are
-  // always marked "final".
 
   final String title;
 
@@ -55,71 +32,179 @@ class MyHomePage extends StatefulWidget {
 }
 
 class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
+  final int level = 3;
 
-  void _incrementCounter() {
-    setState(() {
-      // This call to setState tells the Flutter framework that something has
-      // changed in this State, which causes it to rerun the build method below
-      // so that the display can reflect the updated values. If we changed
-      // _counter without calling setState(), then the build method would not be
-      // called again, and so nothing would appear to happen.
-      _counter++;
-    });
+  List<int> ordinaryList = [];
+  List<int> shuffleList = [];
+
+  @override
+  void initState() {
+    super.initState();
+    startNewGame();
+  }
+
+  void startNewGame() {
+    ordinaryList = getOrdinaryList(level);
+    shuffleList = generateShuffleList(level);
+  }
+
+  bool isFinal(int number) {
+    return number == (level * level - 1);
   }
 
   @override
   Widget build(BuildContext context) {
-    // This method is rerun every time setState is called, for instance as done
-    // by the _incrementCounter method above.
-    //
-    // The Flutter framework has been optimized to make rerunning build methods
-    // fast, so that you can just rebuild anything that needs updating rather
-    // than having to individually change instances of widgets.
     return Scaffold(
-      appBar: AppBar(
-        // TRY THIS: Try changing the color here to a specific color (to
-        // Colors.amber, perhaps?) and trigger a hot reload to see the AppBar
-        // change color while the other colors stay the same.
-        backgroundColor: Theme.of(context).colorScheme.inversePrimary,
-        // Here we take the value from the MyHomePage object that was created by
-        // the App.build method, and use it to set our appbar title.
-        title: Text(widget.title),
+        appBar: AppBar(
+          backgroundColor: Theme.of(context).colorScheme.inversePrimary,
+          title: Text(widget.title),
+        ),
+        body: Container(
+            alignment: Alignment.topCenter,
+            width: MediaQuery.of(context).size.width,
+            child: Column(
+              children: [
+                SizedBox(
+                  width: 600,
+                  height: 600,
+                  child: GridView.count(
+                    primary: false,
+                    padding: const EdgeInsets.all(20),
+                    crossAxisSpacing: 3,
+                    mainAxisSpacing: 3,
+                    crossAxisCount: 3,
+                    children: shuffleList
+                        .map((e) => GestureDetector(
+                              onTap: () => !isFinal(e) ? shift(e) : (),
+                              child: getBox(e, isFinal(e)),
+                            ))
+                        .toList(),
+                  ),
+                ),
+                SizedBox(
+                    width: 600,
+                    child: OverflowBar(
+                      alignment: MainAxisAlignment.spaceEvenly,
+                      children: <Widget>[
+                        OutlinedButton(
+                          style: OutlinedButton.styleFrom(
+                            padding: const EdgeInsets.all(24),
+                            shape: RoundedRectangleBorder(
+                              borderRadius: BorderRadius.circular(24.0),
+                            ),
+                          ),
+                          onPressed: () {
+                            setState(() {
+                              startNewGame();
+                            });
+                          },
+                          child: const Text(
+                            'New Game',
+                            style: TextStyle(
+                                fontWeight: FontWeight.bold, fontSize: 24),
+                          ),
+                        ),
+                        // OutlinedButton(
+                        //   onPressed: () {
+                        //     Navigator.of(context).pop();
+                        //   },
+                        //   child: Text('Back to Github!'),
+                        // ),
+                      ],
+                    ))
+              ],
+            )));
+  }
+
+  Container getBox(int num, bool isFinal) {
+    return Container(
+      width: 200,
+      height: 200,
+      padding: const EdgeInsets.all(8),
+      decoration: BoxDecoration(
+        borderRadius: const BorderRadius.all(Radius.circular(20)),
+        color: isFinal ? Colors.transparent : Colors.teal[100],
       ),
-      body: Center(
-        // Center is a layout widget. It takes a single child and positions it
-        // in the middle of the parent.
-        child: Column(
-          // Column is also a layout widget. It takes a list of children and
-          // arranges them vertically. By default, it sizes itself to fit its
-          // children horizontally, and tries to be as tall as its parent.
-          //
-          // Column has various properties to control how it sizes itself and
-          // how it positions its children. Here we use mainAxisAlignment to
-          // center the children vertically; the main axis here is the vertical
-          // axis because Columns are vertical (the cross axis would be
-          // horizontal).
-          //
-          // TRY THIS: Invoke "debug painting" (choose the "Toggle Debug Paint"
-          // action in the IDE, or press "p" in the console), to see the
-          // wireframe for each widget.
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            const Text(
-              'You have pushed the button this many times:',
+      alignment: Alignment.center,
+      child: Text(
+        isFinal ? '' : (num + 1).toString(),
+        style: const TextStyle(fontWeight: FontWeight.bold, fontSize: 24),
+      ),
+    );
+  }
+
+  List<int> getOrdinaryList(int level) {
+    return List<int>.generate(level * level, (e) => e, growable: true);
+  }
+
+  List<int> generateShuffleList(int level) {
+    List<int> list = getOrdinaryList(level);
+    list.shuffle();
+    return list;
+  }
+
+  void shift(int number) {
+    int index = shuffleList.indexOf(number);
+    if (index - level >= 0 && shuffleList[index - level] == 8) {
+      swap(index, index - level);
+    } else if (index + level <= 8 && shuffleList[index + level] == 8) {
+      swap(index, index + level);
+    } else if ((index % level != 0) && shuffleList[index - 1] == 8) {
+      swap(index, index - 1);
+    } else if (((index + 1) % level != 0) && shuffleList[index + 1] == 8) {
+      swap(index, index + 1);
+    }
+
+    if (checkWin()) {
+      _showMyDialog();
+    }
+  }
+
+  void swap(int preIndex, int nextIndex) {
+    setState(() {
+      var tmp = shuffleList[preIndex];
+      shuffleList[preIndex] = shuffleList[nextIndex];
+      shuffleList[nextIndex] = tmp;
+    });
+  }
+
+  bool checkWin() {
+    return listEquals(shuffleList, ordinaryList);
+  }
+
+  Future<void> _showMyDialog() async {
+    return showDialog<void>(
+      context: context,
+      barrierDismissible: false, // user must tap button!
+      builder: (BuildContext context) {
+        return AlertDialog(
+          title: const Text('Well Done!'),
+          content: const SingleChildScrollView(
+            child: ListBody(
+              children: <Widget>[
+                Text('Would you like to play this N puzzle again?'),
+              ],
             ),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.headlineMedium,
+          ),
+          actions: <Widget>[
+            TextButton(
+              child: const Text('Play again'),
+              onPressed: () {
+                setState(() {
+                  startNewGame();
+                });
+                Navigator.of(context).pop();
+              },
+            ),
+            TextButton(
+              child: const Text('Stop'),
+              onPressed: () {
+                Navigator.of(context).pop();
+              },
             ),
           ],
-        ),
-      ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: const Icon(Icons.add),
-      ), // This trailing comma makes auto-formatting nicer for build methods.
+        );
+      },
     );
   }
 }
